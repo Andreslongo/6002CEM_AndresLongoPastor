8da734bcf9c1272050f37f60ff784bc5f51fbe38
andre <andre@VEGA>
2024-03-07 15:53:41 +0000

    Agregar archivos de proyecto.

---
+++ 6002CEM_AndresLongoPastor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _6002CEM_AndresLongoPastor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
